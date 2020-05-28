9abf7cda4e1be236a4ec15e4cf9fb37821c923e4
Antoine Veuiller <aveuiller@online.net>
2020-05-28 11:03:23 +0200

    chores: resolve maven warnings

--- experiment/run_automated.sh
+++ experiment/run_automated.sh
@@ -6,7 +6,7 @@ LOCAL_JAR=AutomatedBuild.jar
 
 # TODO: assert that 'mvn package' has been run on the project
 if [ ! -e "$LOCAL_JAR" ]; then
-  ln -s ../target/TestFatJarUse.jar $LOCAL_JAR
+  ln -s ../target/TestFatJarUse-jar-with-dependencies.jar $LOCAL_JAR
 fi
 
 java -jar $LOCAL_JAR
